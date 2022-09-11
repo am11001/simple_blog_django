ae1f14e0d89e22863f41d9a1655dd8bff166a6e0
ninja1001 <57629995+ninja1001@users.noreply.github.com>
2022-09-12 02:10:03 +0400

    .

---
+++ blog/posts/views.py
@@ -0,0 +1,17 @@
+from turtle import pos
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+
+
+def posts(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'posts.html', {'posts': posts})
